90802e6686cec353b8e668e3eb5396c816c2e84e
AlanCanalesM <alan.canales@micorreo.upp.edu.mx>
2023-08-29 21:21:21 -0600

    Creates healthCheck endpoint

---
+++ json.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func responseWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to marshal the response %v", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -12,13 +15,37 @@ func main() {
 
 	godotenv.Load()
 
-	fmt.Println("Hola mundo!")
+	portString := os.Getenv("PORT")
 
-	stringPort := os.Getenv("PORT")
+	router := chi.NewRouter()
 
-	if stringPort == "" {
-		log.Fatal("INvalid port :(")
+	router.Use(cors.Handler(
+		cors.Options{
+			AllowedOrigins: []string{"https://*", "http://*"},
+			// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300,
+		},
+	))
+
+	v1 := chi.NewRouter()
+	v1.Get("/health", handlerHealtCheck)
+
+	router.Mount("/v1", v1)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
 	}
 
-	fmt.Println("Port:", stringPort)
+	fmt.Printf("Starting server at port: %v", portString)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Printf("Error starting server %v", err)
+	}
+
 }
